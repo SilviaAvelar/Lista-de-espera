38408f7f745eab80571c8a6f991b51a0eb047adf
Silvia-Avelar <avelar.silvia@yahoo.com>
2025-03-19 15:42:01 +0000

    Criar arquivo vercel.json

--- api/login.js
+++ api/login.js
@@ -9,6 +9,9 @@ const uri = process.env.NODE_ENV === 'production'
     ? process.env.MONGODB_URI // Use a variável de ambiente do Vercel (produção)
     : 'mongodb://localhost:27017/waitlistDB'; // Use o MongoDB local (desenvolvimento)
 
+console.log("URI do MongoDB:", uri); // Adicione esta linha
+
+
 const dbName = 'waitlistDB';
 const secretKey = process.env.JWT_SECRET;
 
@@ -19,13 +22,19 @@ module.exports = async (req, res) => {
         const client = new MongoClient(uri);
 
         try {
+            console.log("Tentando conectar ao MongoDB..."); // Adicione esta linha
+
             await client.connect();
             const db = client.db(dbName);
+            console.log("Conexão com o MongoDB estabelecida com sucesso!"); // Adicione esta linha
+
             const collection = db.collection('users');
 
             const user = await collection.findOne({ username: 'admin' });
 
             if (!user) {
+                console.log("Usuário 'admin' não encontrado."); // Adicione esta linha
+
                 return res.status(401).json({ message: 'Credenciais inválidas' });
             }
 
